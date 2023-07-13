f78754f1f4797936c1fd6362b7d41f9eda504781
Danylo Markovskyi <zenro934@gmail.com>
2023-07-13 21:42:33 +0300

    Файл додавання української у гру

---
+++ LocalizationDatabase_Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO "Languages" VALUES('uk_UA','Українська',null,2,1);
+INSERT INTO "DefaultAudioLanguages" VALUES('uk_UA','English(US)');
+INSERT INTO "SteamLanguages" VALUES('українська','uk_UA');
+INSERT INTO "LanguagePriorities" VALUES('uk_UA','uk_UA',100);
+INSERT INTO "LanguagePriorities" VALUES('uk_UA','en_US',90);
+INSERT INTO "FontStyleSheets" VALUES('uk_UA','Civ6_FontStyles_EFIGS.xml', NULL, 'MinionPro-Medium.otf');
